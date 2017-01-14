da8b1f6d91867f9a24d175e0a1889e3fd605d050
marcelaguiar <marcelaguiar9@gmail.com>
2017-01-14 13:09:02 -0800

    Now using virtualenv

---
+++ flatsnstats/favorite_segments/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FavoriteSegmentsConfig(AppConfig):
+    name = 'favorite_segments'
---
+++ flatsnstats/flatsnstats/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('welcome.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
