cb893bf98101408e766ef3c7f0111e64637fe8b6
marcelaguiar <marcelaguiar9@gmail.com>
2017-02-01 21:46:22 -0800

    Started connection from User Overview to Http Response

--- flatsnstats/flatsnstats/userdata.py
+++ flatsnstats/flatsnstats/userdata.py
@@ -4,8 +4,8 @@ import requests
 
 TOKEN_EX_URL = "https://www.strava.com/oauth/token"
 CLIENT_ID = 15675
-CLIENT_SECRET = ''  # ada13b288862d04f79f6686f84d1ef3127cda3ef
-ACCESS_CODE = ''
+CLIENT_SECRET = 'ada13b288862d04f79f6686f84d1ef3127cda3ef'  # ada13b288862d04f79f6686f84d1ef3127cda3ef
+ACCESS_CODE = 'a3a8949b2223530ace053f9167ad99304d6f2ff9'  # a3a8949b2223530ace053f9167ad99304d6f2ff9
 REDIRECT_URI = 'http://127.0.0.1:8000/'
 
 
@@ -13,13 +13,19 @@ client = Client()
 authorize_url = client.authorization_url(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
 
 payload = {'client_id': 'CLIENT_ID', 'client_secret': 'CLIENT_SECRET', 'code': 'ACCESS_CODE'}
-r = requests.get(TOKEN_EX_URL, params=payload)
+re = requests.get(TOKEN_EX_URL, params=payload)
+jo = re.json()
 
+first_name = jo['firstname']
+last_name = jo['lastname']
+profile_picture = jo['profile']
 
+'''
 class User:
-    first_name = r.json()
-    last_name = "Aguiar"
-    profile_picture = "https://dgalywyr863hv.cloudfront.net/pictures/athletes/2176188/1065064/3/large.jpg"
+    first_name = jo['firstname']
+    last_name = jo['lastname']
+    profile_picture = jo['profile']
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+'''
--- flatsnstats/flatsnstats/views.py
+++ flatsnstats/flatsnstats/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from . import userdata
+
+
+first_name = userdata.first_name
+last_name = userdata.last_name
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'first_name': first_name,
+        'last_name': last_name
+    })
 
 
 def handler404(request):
@@ -11,23 +19,3 @@ def handler404(request):
 
 def handler500(request):
     return render(request, '500.html')
-
-
-'''
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    render
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
-'''
