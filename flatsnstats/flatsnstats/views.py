36b849e06e21904dcd1e69bb93c96ed46f5412ed
marcelaguiar <marcelaguiar9@gmail.com>
2017-02-25 22:52:30 -0800

    MAJOR BREAKTHROUGH
    Figured out how to grab the returned access token. With this, everything
    fell into place, and design was simplified. Still much to do however.

--- flatsnstats/flatsnstats/views.py
+++ flatsnstats/flatsnstats/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
-from . import userdata
+from stravalib.client import Client
 
-
-first_name = userdata.first_name
-last_name = userdata.last_name
+client = Client()
 
 
 def index(request):
-    return render(request, 'index.html', {
-        'first_name': first_name,
-        'last_name': last_name
-    })
+
+    access_token = get_access_token(request)
+    client.access_token = access_token
+    athlete = client.get_athlete()
+    athlete_data = {
+        'first_name': athlete.firstname,
+        'last_name': athlete.lastname,
+        'profile_picture': athlete.profile
+    }
+
+    return render(request, 'index.html', athlete_data)
+
+
+def get_access_token(request):
+    client_id = 15675
+    client_secret = 'ada13b288862d04f79f6686f84d1ef3127cda3ef'
+    access_code = request.GET.get('code')
+    access_token = client.exchange_code_for_token(client_id=client_id, client_secret=client_secret, code=access_code)
+
+    return access_token
 
 
 def handler404(request):
