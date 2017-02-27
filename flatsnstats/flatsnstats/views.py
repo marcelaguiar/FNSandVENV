4ec63557a84825ca122a5269ff4bc11e29c5d178
marcelaguiar <marcelaguiar9@gmail.com>
2017-02-26 18:50:30 -0800

    Top riding partners added!
    Takes 5 minutes for API calls, but it checks out!

--- flatsnstats/flatsnstats/views.py
+++ flatsnstats/flatsnstats/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from stravalib.client import Client
 
+
 client = Client()
 
 
 def index(request):
-
     access_token = get_access_token(request)
     client.access_token = access_token
     athlete = client.get_athlete()
+
+    sorted_partner_list = top_training_partners(client)
+
     athlete_data = {
+        'id': athlete.id,
         'first_name': athlete.firstname,
         'last_name': athlete.lastname,
-        'profile_picture': athlete.profile
+        'profile_picture': athlete.profile,
+        'training_partners': sorted_partner_list
     }
 
     return render(request, 'index.html', athlete_data)
@@ -25,6 +30,26 @@ def get_access_token(request):
     access_token = client.exchange_code_for_token(client_id=client_id, client_secret=client_secret, code=access_code)
 
     return access_token
+
+
+def top_training_partners(c):
+    my_dict = {}
+
+    for activity in c.get_activities():
+        for related_activity in activity.related:
+            if related_activity.athlete.id in my_dict:
+                my_dict[related_activity.athlete.id] += 1
+            else:
+                my_dict[related_activity.athlete.id] = 1
+
+    ten_sorted = sorted([(k, v) for k, v in my_dict.items()], key=lambda x: x[1], reverse=True)[0:10]
+
+    athlete_list = []
+    for person in ten_sorted:
+        athlete = c.get_athlete(person[0])
+        athlete_list.append(athlete.firstname + ' ' + athlete.lastname + ' (' + str(person[1]) + ')')
+
+    return athlete_list
 
 
 def handler404(request):
