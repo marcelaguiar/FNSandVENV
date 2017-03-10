d7a60c2bf39bb16c00fd19a14b7e8a1077abd84f
marcelaguiar <marcelaguiar9@gmail.com>
2017-03-10 11:55:42 -0800

    added Ajax + Javascript to update button

--- flatsnstats/flatsnstats/views.py
+++ flatsnstats/flatsnstats/views.py
@@ -16,7 +16,7 @@ def strava_site(request):
 
 
 def home(request):
-    sorted_partner_list = calc_top_training_partners(client)
+    sorted_partner_list = compile_top_training_partners(client)
 
     athlete_data = {
         'id': current_athlete.id,
@@ -38,26 +38,27 @@ def most_ridden_segments(request):
 
 
 def top_training_partners(request):
+    if request.method == 'POST':
+        update_top_training_partners(request)
+    else:
+        sorted_partner_list = compile_top_training_partners(client)
+        try:
+            ttp_object = TopTrainingPartners.objects.get(strava_id=current_id)
+            last_updated = getattr(ttp_object, "last_updated")
+        except ObjectDoesNotExist:
+            last_updated = 'unknown'
+            print('TTP: Could not find user in database')
 
-    sorted_partner_list = calc_top_training_partners(client)
+        athlete_data = {
+            'id': current_athlete.id,
+            'first_name': current_athlete.firstname,
+            'last_name': current_athlete.lastname,
+            'profile_picture': current_athlete.profile,
+            'training_partners': sorted_partner_list,
+            'last_updated': last_updated
+        }
 
-    try:
-        ttp_object = TopTrainingPartners.objects.get(strava_id=current_id)
-        last_updated = getattr(ttp_object, "last_updated")
-    except ObjectDoesNotExist:
-        last_updated = 'unknown'
-        print('TTP: Could not find user in database')
-
-    athlete_data = {
-        'id': current_athlete.id,
-        'first_name': current_athlete.firstname,
-        'last_name': current_athlete.lastname,
-        'profile_picture': current_athlete.profile,
-        'training_partners': sorted_partner_list,
-        'last_updated': last_updated
-    }
-
-    return render(request, 'top_training_partners/index.html', athlete_data)
+        return render(request, 'top_training_partners/index.html', athlete_data)
 
 
 def welcome(request):
@@ -104,7 +105,7 @@ def temporary_redirect(request):
     return redirect('/')
 
 
-def calc_top_training_partners(c):
+def compile_top_training_partners(c):
     athlete_list = []
 
     try:
@@ -113,34 +114,63 @@ def calc_top_training_partners(c):
             field = 'partner' + str(i)
             athlete_list.append(getattr(athlete_object, field))
     except ObjectDoesNotExist:
-        my_dict = {}
-        for activity in c.get_activities():
-            for related_activity in activity.related:
-                if related_activity.athlete.id in my_dict:
-                    my_dict[related_activity.athlete.id] += 1
-                else:
-                    my_dict[related_activity.athlete.id] = 1
+        athlete_list = calc_top_training_partners(c)
+    return athlete_list
 
-        ten_sorted = sorted([(k, v) for k, v in my_dict.items()], key=lambda x: x[1], reverse=True)[0:10]
 
-        for person in ten_sorted:
-            partner = c.get_athlete(person[0])
-            athlete_list.append(partner.firstname + ' ' + partner.lastname + ' (' + str(person[1]) + ')')
+def calc_top_training_partners(c):
+    my_dict = {}
+    athlete_list = []
 
-        t = TopTrainingPartners(strava_id=current_id, last_updated=time.time(),
-                                partner1=list_access_helper(athlete_list, 0),
-                                partner2=list_access_helper(athlete_list, 1),
-                                partner3=list_access_helper(athlete_list, 2),
-                                partner4=list_access_helper(athlete_list, 3),
-                                partner5=list_access_helper(athlete_list, 4),
-                                partner6=list_access_helper(athlete_list, 5),
-                                partner7=list_access_helper(athlete_list, 6),
-                                partner8=list_access_helper(athlete_list, 7),
-                                partner9=list_access_helper(athlete_list, 8),
-                                partner10=list_access_helper(athlete_list, 9))
-        t.save()
+    for activity in c.get_activities():
+        for related_activity in activity.related:
+            if related_activity.athlete.id in my_dict:
+                my_dict[related_activity.athlete.id] += 1
+            else:
+                my_dict[related_activity.athlete.id] = 1
+
+    ten_sorted = sorted([(k, v) for k, v in my_dict.items()], key=lambda x: x[1], reverse=True)[0:10]
+
+    for person in ten_sorted:
+        partner = c.get_athlete(person[0])
+        athlete_list.append(partner.firstname + ' ' + partner.lastname + ' (' + str(person[1]) + ')')
+
+    t = TopTrainingPartners(strava_id=current_id, last_updated=time.time(),
+                            partner1=list_access_helper(athlete_list, 0),
+                            partner2=list_access_helper(athlete_list, 1),
+                            partner3=list_access_helper(athlete_list, 2),
+                            partner4=list_access_helper(athlete_list, 3),
+                            partner5=list_access_helper(athlete_list, 4),
+                            partner6=list_access_helper(athlete_list, 5),
+                            partner7=list_access_helper(athlete_list, 6),
+                            partner8=list_access_helper(athlete_list, 7),
+                            partner9=list_access_helper(athlete_list, 8),
+                            partner10=list_access_helper(athlete_list, 9))
+    t.save()
 
     return athlete_list
+
+
+def update_top_training_partners(request):
+
+    sorted_partner_list = calc_top_training_partners(client)
+    try:
+        ttp_object = TopTrainingPartners.objects.get(strava_id=current_id)
+        last_updated = getattr(ttp_object, "last_updated")
+    except ObjectDoesNotExist:
+        last_updated = 'unknown'
+        print('TTP: Could not find user in database')
+
+    athlete_data = {
+        'id': current_athlete.id,
+        'first_name': current_athlete.firstname,
+        'last_name': current_athlete.lastname,
+        'profile_picture': current_athlete.profile,
+        'training_partners': sorted_partner_list,
+        'last_updated': last_updated
+    }
+
+    return render(request, 'top_training_partners/index.html', athlete_data)
 
 
 def list_access_helper(my_list, my_index):
