00120bf467f7db95c42c5eb9278d3a1522cbe403
marcelaguiar <marcelaguiar9@gmail.com>
2017-03-14 14:48:12 -0700

    Changed database design, but currently have datetime issues

--- flatsnstats/flatsnstats/models.py
+++ flatsnstats/flatsnstats/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+import datetime
+
+
+class Users(models.Model):
+    strava_id = models.IntegerField(default=0)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
+    authorized = models.BooleanField(default=False)
+    ttp_last_updated = models.DateTimeField(auto_now=False, default=None)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' (' + str(self.strava_id) + "): " + str(self.authorized)
 
 
 class TopTrainingPartners(models.Model):
@@ -16,11 +28,10 @@ class TopTrainingPartners(models.Model):
     partner10 = models.CharField(max_length=100)
 
 
-class Users(models.Model):
-    strava_id = models.IntegerField(default=0)
-    first_name = models.CharField(max_length=100, default='')
-    last_name = models.CharField(max_length=100, default='')
-    authorized = models.BooleanField(default=False)
+class Relationship(models.Model):
+    user1 = models.IntegerField(default=0)
+    user2 = models.IntegerField(default=0)
+    ra_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name + ' (' + str(self.strava_id) + "): " + str(self.authorized)
+        return str(self.user1) + ' - ' + str(self.user2) + ": " + str(self.ra_count)
--- flatsnstats/flatsnstats/views.py
+++ flatsnstats/flatsnstats/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F
 from stravalib.client import Client
-from .models import TopTrainingPartners, Users
-import time
+from .models import TopTrainingPartners, Users, Relationship
+import datetime
 
 
 client = Client()
@@ -16,14 +17,12 @@ def strava_site(request):
 
 
 def home(request):
-    sorted_partner_list = compile_top_training_partners(client)
-
     athlete_data = {
         'id': current_athlete.id,
         'first_name': current_athlete.firstname,
         'last_name': current_athlete.lastname,
         'profile_picture': current_athlete.profile,
-        'training_partners': sorted_partner_list
+        'training_partners': compile_top_training_partners(client)
     }
 
     return render(request, 'index.html', athlete_data)
@@ -65,7 +64,8 @@ def set_global_athlete(request):
         u = Users(strava_id=current_id,
                   first_name=current_athlete.firstname,
                   last_name=current_athlete.lastname,
-                  authorized=True)
+                  authorized=True,
+                  ttp_last_updated=datetime.datetime.min)
         u.save()
 
         user_authorized = True
@@ -86,8 +86,7 @@ def temporary_redirect(request):
 
 def top_training_partners(request):
     if request.POST.get('button_click'):
-        print("LLLLLLMAO")
-        athlete_data = update_top_training_partners(request)
+        athlete_data = update_top_training_partners()
     else:
         athlete_data = {
             'id': current_athlete.id,
@@ -104,13 +103,13 @@ def top_training_partners(request):
 def compile_top_training_partners(c):
     athlete_list = []
 
-    try:
+    '''try:
         athlete_object = TopTrainingPartners.objects.get(strava_id=current_id)
         for i in range(1, 11):
             field = 'partner' + str(i)
             athlete_list.append(getattr(athlete_object, field))
-    except ObjectDoesNotExist:
-        athlete_list = calc_top_training_partners(c)
+    except ObjectDoesNotExist:'''
+    athlete_list = calc_top_training_partners(c)
     return athlete_list
 
 
@@ -119,20 +118,60 @@ def calc_top_training_partners(c):
     my_dict = {}
     athlete_list = []
 
-    for activity in c.get_activities():
-        for related_activity in activity.related:
-            if related_activity.athlete.id in my_dict:
-                my_dict[related_activity.athlete.id] += 1
-            else:
-                my_dict[related_activity.athlete.id] = 1
+    try:
+        user_object = Users.objects.get(strava_id=current_id)
+        last_updated = getattr(user_object, "ttp_last_updated")
+    except ObjectDoesNotExist:
+        print("ERROR: User doesn't exist. Look into this!!!!!!!!! ")
+        return None
 
-    ten_sorted = sorted([(k, v) for k, v in my_dict.items()], key=lambda x: x[1], reverse=True)[0:10]
+    # TODO: FIGURE OUT WHAT TO SET last_updated TO
+    print(str(last_updated.isoformat()).replace('+00:00', 'Z'))
+    print("2020-01-01T00:00:00Z")
+
+    for activity in c.get_activities(after=last_updated):
+        for related_activity in activity.related:
+            training_partner_id = related_activity.athlete.id
+            '''
+            if training_partner_id in my_dict:
+                my_dict[training_partner_id] += 1
+            else:
+                my_dict[training_partner_id] = 1'''
+
+            # Update or create Relationship in db (maybe switch to update_or_create)
+            try:
+                db_object = Relationship.objects.get(user1=current_id, user2=training_partner_id)
+                db_object.ra_count = F("ra_count") + 1
+                db_object.save(update_fields=['ra_count'])
+            except Relationship.DoesNotExist:
+                r = Relationship(user1=current_id, user2=training_partner_id, ra_count=1)
+                r.save()
+            except Relationship.MultipleObjectsReturned:
+                print("ERROR: Marcel, Why are there repeat partner pairs!?!?!? ")
+
+    # Update User.tpp_last_updated
+    try:
+        db_object = Users.objects.get(strava_id=current_id)
+        db_object.ttp_last_updated = datetime.datetime.min
+        db_object.save(update_fields=['ttp_last_updated'])
+    except Users.DoesNotExist:
+        print('ERROR: Well this is awkward, this shouldnt have happened. #1')
+    except Relationship.MultipleObjectsReturned:
+        print('ERROR: Well this is awkward, this shouldnt have happened. #2')
+
+    ttp_qs = Relationship.objects.filter(user1=current_id).order_by('-ra_count')[:10]
+
+    for relationship in ttp_qs:
+        partner = c.get_athlete(relationship.user2)
+        athlete_list.append(partner.firstname + ' ' + partner.lastname + ' (' + str(relationship.ra_count) + ')')
+
+    '''ten_sorted = sorted([(k, v) for k, v in my_dict.items()], key=lambda x: x[1], reverse=True)[0:10]
 
     for person in ten_sorted:
         partner = c.get_athlete(person[0])
         athlete_list.append(partner.firstname + ' ' + partner.lastname + ' (' + str(person[1]) + ')')
 
-    t = TopTrainingPartners(strava_id=current_id, last_updated=time.time(),
+    t = TopTrainingPartners(strava_id=current_id, last_updated=datetime.datetime(),
                             partner1=list_access_helper(athlete_list, 0),
                             partner2=list_access_helper(athlete_list, 1),
                             partner3=list_access_helper(athlete_list, 2),
@@ -143,12 +182,12 @@ def calc_top_training_partners(c):
                             partner8=list_access_helper(athlete_list, 7),
                             partner9=list_access_helper(athlete_list, 8),
                             partner10=list_access_helper(athlete_list, 9))
-    t.save()
+    t.save()'''
 
     return athlete_list
 
 
-def update_top_training_partners(request):
+def update_top_training_partners():
     # TODO: Calculate activities after certain date
     # 1. Get current top 10 as {strava_id:number_of_related_activities}
     # 2. Get last_updated
